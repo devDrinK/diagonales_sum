afe55907a166e027e5656f38a6a9bd7f325826f4
devjpunk <91user101@protonmail.com>
2024-11-24 19:59:21 -0600

    suma de diagonales matriz

---
+++ diagonales/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace diagonales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtMatriz = new System.Windows.Forms.TextBox();
+            this.lblDprincipal = new System.Windows.Forms.Label();
+            this.lblDopuesta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNum.Location = new System.Drawing.Point(393, 12);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 26);
+            this.txtNum.TabIndex = 0;
+            this.txtNum.Click += new System.EventHandler(this.txtNum_Click);
+            this.txtNum.TextChanged += new System.EventHandler(this.txtNum_TextChanged);
+            // 
+            // txtMatriz
+            // 
+            this.txtMatriz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtMatriz.Location = new System.Drawing.Point(393, 44);
+            this.txtMatriz.Name = "txtMatriz";
+            this.txtMatriz.Size = new System.Drawing.Size(100, 26);
+            this.txtMatriz.TabIndex = 1;
+            this.txtMatriz.TextChanged += new System.EventHandler(this.txtMatriz_TextChanged);
+            // 
+            // lblDprincipal
+            // 
+            this.lblDprincipal.AutoSize = true;
+            this.lblDprincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDprincipal.Location = new System.Drawing.Point(77, 18);
+            this.lblDprincipal.Name = "lblDprincipal";
+            this.lblDprincipal.Size = new System.Drawing.Size(0, 20);
+            this.lblDprincipal.TabIndex = 2;
+            this.lblDprincipal.Click += new System.EventHandler(this.lblDprincipal_Click);
+            // 
+            // lblDopuesta
+            // 
+            this.lblDopuesta.AutoSize = true;
+            this.lblDopuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDopuesta.Location = new System.Drawing.Point(537, 18);
+            this.lblDopuesta.Name = "lblDopuesta";
+            this.lblDopuesta.Size = new System.Drawing.Size(0, 20);
+            this.lblDopuesta.TabIndex = 3;
+            this.lblDopuesta.Click += new System.EventHandler(this.lblDopuesta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.lblDopuesta);
+            this.Controls.Add(this.lblDprincipal);
+            this.Controls.Add(this.txtMatriz);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtMatriz;
+        private System.Windows.Forms.Label lblDprincipal;
+        private System.Windows.Forms.Label lblDopuesta;
+    }
+}
+
---
+++ diagonales/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace diagonales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random x = new Random();
+        int anchoCelda = 30;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNum.Multiline = false;
+            txtMatriz.Multiline = true;
+            txtMatriz.WordWrap = false;
+            txtMatriz.ReadOnly = true;
+            txtMatriz.Font = new Font("Courier New", 12);
+        }
+
+        float factorAncho = 1.7f; // Puedes ajustar este factor
+        float factorAlto = 1.2f;
+        private async void txtNum_Click(object sender, EventArgs e)
+        {
+            txtMatriz.Clear();
+
+            if (int.TryParse(txtNum.Text, out int def) && def > 0)
+            {
+                string[,] diagonales = new string[def,def];
+                int[,] matriz = new int[def, def];
+                int altoCelda = 30;
+                int margen = 20;
+
+                txtMatriz.Width = (int)((def * anchoCelda + margen) * factorAncho);
+                txtMatriz.Height = (int)((def * altoCelda + margen) * factorAlto);
+
+                
+
+                // Generar la matriz con valores aleatorios
+                for (int i = 0; i < def; i++)
+                {
+                    for (int j = 0; j < def; j++)
+                    {
+                        matriz[i, j] = x.Next(50);
+                    }
+                }
+                CenterTextBox();
+                int sumaDiagonalPrincipal = 0;
+                int sumaDiagonalSecundaria = 0;
+                int p = matriz.GetLength(0);
+                // Imprimir la matriz original
+                for (int i = 0; i < def; i++)
+                {
+                    string row = "| ";
+                    sumaDiagonalPrincipal += matriz[i, i];
+                    sumaDiagonalSecundaria += matriz[i, p - i - 1];
+                    for (int j = 0; j < def; j++)
+                    {
+                        row += matriz[i, j].ToString("D2") + " | ";
+                    }
+                    txtMatriz.AppendText(row + Environment.NewLine);
+
+                    if (i < def - 1)
+                    {
+                        string separator = "|";
+                        for (int j = 0; j < def; j++)
+                        {
+                            separator += new string('_', 4) + "|";
+                        }
+                        txtMatriz.AppendText(separator + Environment.NewLine);
+                    }
+
+                    await Task.Delay(500);
+                }
+
+                await Task.Delay(1000);
+                
+                txtMatriz.Clear();
+                // Imprimir solo las diagonales con el formato deseado
+                for (int i = 0; i < def; i++)
+                {
+                    string row = "| ";
+                    for (int j = 0; j < def; j++)
+                    {
+                        if (i == j || j == def - i - 1) // Verificar si es parte de alguna de las diagonales
+                        {
+                            row += matriz[i, j].ToString("D2").PadLeft(2).PadRight(2) + " | ";
+                        }
+                        else
+                        {
+                            row += "   | "; // Espacio en blanco para alinear correctamente
+                        }
+                    }
+                    txtMatriz.AppendText(row + Environment.NewLine);
+
+                    if (i < def - 1)
+                    {
+                        string separator = "|";
+                        for (int j = 0; j < def; j++)
+                        {
+                            separator += new string('_', 4) + "|";
+                        }
+                        txtMatriz.AppendText(separator + Environment.NewLine);
+                    }
+
+                    await Task.Delay(500); // Simular un retraso para efectos visuales
+                }
+
+                lblDprincipal.Text = $"La suma de la principal es: {sumaDiagonalPrincipal}";
+                lblDopuesta.Text = $"La suma de la opuesta es: {sumaDiagonalSecundaria}";
+            }
+            else if (txtNum.Text == "")
+            { }
+            else
+            {
+                MessageBox.Show("Ingresa solo números enteros positivos.");
+            }
+        }
+
+        private void CenterTextBox()
+        {
+            txtMatriz.Left = (this.ClientSize.Width - txtMatriz.Width) / 2;
+            txtMatriz.Top = (this.ClientSize.Height - txtMatriz.Height) / 2;
+        }
+
+        private void txtMatriz_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDprincipal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDopuesta_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
